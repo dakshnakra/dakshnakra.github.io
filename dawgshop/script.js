447d495d933d4ccb7d4ad235dba8c3eec868f41e
daksh <daksh.nakra@gmail.com>
2017-06-29 18:26:05 +0000

    green text

---
+++ dawgshop/script.js
@@ -0,0 +1,6 @@
+document.getElementById('go').onclick = function() {
+var inputText = $('#text-input').val();
+var dawgifiedText = inputText + ", lennygames12";
+document.getElementById('output').innerHTML = dawgifiedText;
+};
+
